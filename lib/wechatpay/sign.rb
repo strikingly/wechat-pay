f44127344ac0f01dbdfc8da40f476e787c2bc65a
Yixian Zhao <462377353@qq.com>
2018-03-06 15:11:04 +0800

    Fix: eliminate the effect of empty fileds

--- lib/wechatpay/sign.rb
+++ lib/wechatpay/sign.rb
@@ -1,7 +1,7 @@
 module Wechatpay
   module Sign
     def self.md5(params)
-      str = params.sort.map{ |i| i.join("=") }.join('&')
+      str = params.select{ |k,v| v.present? }.sort.map{ |i| i.join("=") }.join('&')
       str << "&key=#{Wechatpay::Config.payment_key}"
       Digest::MD5.hexdigest(str).upcase
     end
